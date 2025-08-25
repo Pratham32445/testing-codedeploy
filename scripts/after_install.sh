f9757044d4c4603f568f22b1b834695303fd14e2
Pratham mehta <mehtapratham49@gmail.com>
2025-08-25 15:20:23 +0530

    updated the after_install.sh

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -3,6 +3,14 @@ set -e
 
 cd /home/ubuntu/testing-codedeploy
 
+# Load NVM (so node and npm are available)
+export NVM_DIR="/home/ubuntu/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+
+echo "Using Node version:"
+node -v
+npm -v
+
 echo "Installing dependencies..."
 npm install --production
 
