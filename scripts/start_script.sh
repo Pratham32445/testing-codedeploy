f484d8362dfd04caec16cca3b271425f2e50b2e7
Pratham mehta <mehtapratham49@gmail.com>
2025-08-25 14:53:46 +0530

    changes in scripts

--- scripts/start_script.sh
+++ scripts/start_script.sh
@@ -4,8 +4,8 @@ set -e
 cd /home/ubuntu/testing-codedeploy
 
 # Stop existing PM2 process if running
-pm2 delete testing-app || true
+pm2 delete testing-codedeploy || true
 
 # Start the built app with PM2
-pm2 start dist/index.js --name "testing-app"
+pm2 start dist/index.js --name "testing-codedeploy"
 pm2 save
